16ecd962343ab2d7ecc54d9453a223591823d1ec
Tekyto <Tekyto2014@gmail.com>
2022-11-17 12:27:18 +0200

    variables

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function isLeap(year) {
-	if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+	if (year % 4 === 0 && year % 100 !== 0){
+		return true;
+	} else if (year % 400 === 0){
 		return true;
 	} else {
 		return false;
